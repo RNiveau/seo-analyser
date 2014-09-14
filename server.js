4d18c33afe38ecc325b02664e07f878b153d7e23
Romain Niveau <romain.niveau@gmail.com>
2014-09-14 23:52:35 +0200

    next

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var url = require('url');
 
 var server = http.createServer(function (request, response) {
 
@@ -15,14 +16,24 @@ var server = http.createServer(function (request, response) {
     // the end event tells you that you have entire body
     request.on('end', function () {
         try {
+            console.log(body);
+            console.log(request.method);
             var data = JSON.parse(body);
+            http.get(data.url, function (res) {
+                res.on("end", function (body) {
+                   console.log("yes");
+                });
+                res.on("onBody", function (body) {
+                    response.end(body);
+                });
+            });
         } catch (er) {
+            console.log("error" + er);
             // uh oh!  bad json!
             response.statusCode = 400;
             return response.end('error: ' + er.message);
         }
 
     });
-    response.end();
 });
 server.listen(8080, "localhost");
